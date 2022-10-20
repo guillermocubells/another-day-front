0c0e1bb4a8747156740d24d0d4ffc68f697a372a
poncho678 <vincent.tollens@googlemail.com>
2022-10-20 22:19:28 +0200

    feat: added apiclient

---
+++ src/services/api-client.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5005";
+
+const apiClient = axios.create({
+  baseURL,
+});
+
+apiClient.interceptors.request.use((config) => {
+  const storedToken = localStorage.getItem("authToken");
+
+  if (storedToken) {
+    config.headers = { Authorization: `Bearer ${storedToken}` };
+  }
+
+  return config;
+});
+
+export default apiClient;
