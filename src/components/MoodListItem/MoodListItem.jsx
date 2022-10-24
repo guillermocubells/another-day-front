f768c107f12e777520764e843a31b31e42887e61
poncho678 <vincent.tollens@googlemail.com>
2022-10-24 17:40:16 +0200

    fix: MoodListItem not showing date

--- src/components/MoodListItem/MoodListItem.jsx
+++ src/components/MoodListItem/MoodListItem.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function MoodListItem({ mood }) {
-  const { date, status, _id } = mood;
+  const { date, status, _id, activities } = mood;
+  const newDate = new Date(date);
   const navigate = useNavigate();
 
   return (
     <article onClick={() => navigate(`/mood/${_id}`)}>
-      <div>{date}</div>
+      <div>{newDate.toDateString()}</div>
       <div>{status}</div>
+      <div>{activities}</div>
     </article>
   );
 }
