74382fe634168e5a6735b72b79c42871ead7e7a8
poncho678 <vincent.tollens@googlemail.com>
2022-10-25 19:28:14 +0200

    cleaned up homepage

--- src/components/MoodCheckInForm/MoodCheckInForm.jsx
+++ src/components/MoodCheckInForm/MoodCheckInForm.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
-import apiClient from "../../services/api-client";
-
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function MoodCheckInForm({ activities, mood_status, mood_substatus }) {
-  console.log(activities, mood_status, mood_substatus);
+import apiClient from "../../services/api-client";
+import Loading from "../../components/Loading/Loading";
+
+function MoodCheckInForm() {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const [moodData, setMoodData] = useState({
+    mood_status: [],
+    mood_substatus: [],
+    activities: [],
+  });
 
   const [form, setForm] = useState({
     status: "",
@@ -15,7 +21,27 @@ function MoodCheckInForm({ activities, mood_status, mood_substatus }) {
     date: new Date(),
     image: "",
   });
-  console.log(form);
+
+  // Getting Mood Data
+  useEffect(() => {
+    setIsLoading(true);
+    apiClient
+      .get("/api")
+      .then((res) => {
+        setMoodData(res.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.error(error);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  const { mood_status, mood_substatus, activities } = moodData;
 
   function handleChange(evt) {
     const { name, value } = evt.target;
@@ -41,7 +67,6 @@ function MoodCheckInForm({ activities, mood_status, mood_substatus }) {
         navigate(`/mood/${data._id}`);
       })
       .catch((err) => {
-        console.log("yeppp?");
         if (err?.response?.data.code === 1) {
           // navigate
           return navigate(`/mood/${err.response.data._id}`);
@@ -51,91 +76,94 @@ function MoodCheckInForm({ activities, mood_status, mood_substatus }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      {/* TODO! Make datetime display current time as default  */}
-      <label>
-        Date
-        <input
-          type="datetime-local"
-          name="date"
-          onChange={handleChange}
-          value={form.date}
-        ></input>
-      </label>
-      <br />
-      {/* Setting Up Mood Selection */}
-      {mood_status.map((status, index) => {
-        return (
-          <label key={index}>
-            {status}
-            <input
-              type="radio"
-              value={status}
-              name="status"
-              onChange={handleChange}
-            />{" "}
-          </label>
-        );
-      })}
-
-      {/* If a Mood has been selected, show relative substatus */}
-      {form.status && <br />}
-      {form.status &&
-        mood_substatus[form.status].map((item, index) => {
+    <>
+      <h1>Mood Check</h1>
+      <form onSubmit={handleSubmit}>
+        {/* TODO! Make datetime display current time as default  */}
+        <label>
+          Date
+          <input
+            type="datetime-local"
+            name="date"
+            onChange={handleChange}
+            value={form.date}
+          ></input>
+        </label>
+        <br />
+        {/* Setting Up Mood Selection */}
+        {mood_status.map((status, index) => {
           return (
             <label key={index}>
-              {item}
+              {status}
               <input
                 type="radio"
-                value={item}
-                name="substatus"
+                value={status}
+                name="status"
                 onChange={handleChange}
               />{" "}
             </label>
           );
         })}
-      <br />
-      {/* Get Activities. 
+
+        {/* If a Mood has been selected, show relative substatus */}
+        {form.status && <br />}
+        {form.status &&
+          mood_substatus[form.status].map((item, index) => {
+            return (
+              <label key={index}>
+                {item}
+                <input
+                  type="radio"
+                  value={item}
+                  name="substatus"
+                  onChange={handleChange}
+                />{" "}
+              </label>
+            );
+          })}
+        <br />
+        {/* Get Activities. 
       TODO! create array from selected values
       TODO! create the option to create a new activity. */}
-      {activities.map((activity, index) => {
-        return (
-          <label key={index}>
-            {activity}
-            <input
-              type="checkbox"
-              name="activities"
-              id={activity}
-              onChange={handleCheckbox}
-              checked={form.activities.id}
-            ></input>
-          </label>
-        );
-      })}
+        {activities.map((activity, index) => {
+          return (
+            <label key={index}>
+              {activity}
+              <input
+                type="checkbox"
+                name="activities"
+                id={activity}
+                onChange={handleCheckbox}
+                checked={form.activities.id}
+              ></input>
+            </label>
+          );
+        })}
 
-      <br />
-      <label>
-        Journal
-        <textarea
-          type="text"
-          name="description"
-          onChange={handleChange}
-          value={form.description}
-        ></textarea>
-      </label>
-      <br />
-      <label>
-        Image
-        <input
-          type="file"
-          name="image"
-          onChange={handleChange}
-          value={form.image}
-        ></input>
-      </label>
-      <br />
-      <button type="submit">Check In</button>
-    </form>
+        <br />
+        <label>
+          Journal
+          <textarea
+            type="text"
+            name="description"
+            onChange={handleChange}
+            value={form.description}
+          ></textarea>
+        </label>
+        <br />
+        <label>
+          Image
+          <input
+            type="file"
+            name="image"
+            onChange={handleChange}
+            value={form.image}
+          ></input>
+        </label>
+        <br />
+        <button type="submit">Check In</button>
+      </form>
+    </>
   );
 }
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,45 +1,12 @@
 import "./HomePage.css";
-import React, { useEffect, useState } from "react";
-import apiClient from "../../services/api-client";
-import Loading from "../../components/Loading/Loading";
+import React from "react";
 import MoodCheckInForm from "../../components/MoodCheckInForm/MoodCheckInForm";
 import MoodList from "../../components/MoodList/MoodList";
 
 function HomePage() {
-  const [moodData, setMoodData] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-
-  // Getting Mood Data
-  useEffect(() => {
-    setIsLoading(true);
-    apiClient
-      .get("/api")
-      .then((res) => {
-        setMoodData(res.data);
-        console.log(res.data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        console.error(error);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return (
     <>
-      <div>
-        <h1>Mood Check</h1>
-        <br />
-        <MoodCheckInForm
-          activities={moodData?.activities}
-          mood_status={moodData?.mood_status}
-          mood_substatus={moodData?.mood_substatus}
-        />
-      </div>
+      <MoodCheckInForm />
       <MoodList />
     </>
   );
