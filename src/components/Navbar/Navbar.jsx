6551b6b506ed0846b8960f402344e99eca0f9bda
poncho678 <vincent.tollens@googlemail.com>
2022-10-29 16:29:38 +0200

    changing order and structure of the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,24 +10,20 @@ function Navbar() {
 
   return (
     <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+      <Link to="/">Gemma! or... Another Day.</Link>
 
       {isLoggedIn && (
         <>
-          <button onClick={logOutUser}>Logout</button>
-
+          <Link to="/">
+            <button>Check In</button>
+          </Link>
           <Link to="/profile">
             <button>Profile</button>
-            {/* <img src="https://picsum.photos/id/402/200/300" style={{ width: 50, height: 50, borderRadius: 25}} alt="profile" /> */}
           </Link>
           <Link to="/dashboard">
             <button>Dashboard</button>
-            {/* <img src="https://picsum.photos/id/402/200/300" style={{ width: 50, height: 50, borderRadius: 25}} alt="profile" /> */}
           </Link>
-
-          <span>{user && user.name}</span>
+          <button onClick={logOutUser}>Logout</button>
         </>
       )}
 
