bce039b4c639a9638f45a7bf7131db8c030c053a
poncho678 <vincent.tollens@googlemail.com>
2022-10-30 19:55:32 +0100

    fixed api client not using the right environmental variables

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5005";
+const baseURL = process.env.REACT_APP_SERVER_URL || "http://localhost:5005";
 
 const apiClient = axios.create({
   baseURL,
