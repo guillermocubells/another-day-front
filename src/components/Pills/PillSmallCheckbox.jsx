d847e587faa7947ec967dc8974dd52152a8d8394
poncho678 <vincent.tollens@googlemail.com>
2022-11-05 17:58:27 +0100

    added a pill checkbox component

--- src/components/Pills/PillSmallCheckbox.jsx
+++ src/components/Pills/PillSmallCheckbox.jsx
@@ -1,9 +1,32 @@
-import "./PillSmallCheckbox.css";
+import "./PillSmall.css";
 
 import React from "react";
 
-function PillSmallCheckbox() {
-  return <div>PillSmallCheckbox</div>;
+function PillSmallCheckbox({ title, handleCheckbox, checked }) {
+  function checkIfThereIsACheckedProperty() {
+    if (checked === true) {
+      return "active";
+    } else if (checked === "") {
+      return;
+    } else {
+      return "inactive";
+    }
+  }
+
+  return (
+    <label
+      className={`pill-small pill-checkbox ${checkIfThereIsACheckedProperty()}`}
+    >
+      {title}
+      <input
+        type="checkbox"
+        name="activities"
+        id={title}
+        onChange={handleCheckbox}
+        checked={checked}
+      ></input>
+    </label>
+  );
 }
 
 export default PillSmallCheckbox;
