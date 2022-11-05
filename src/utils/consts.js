6e97c48f02ed19d70a6ada7c2d262a22c1f4f57a
poncho678 <vincent.tollens@googlemail.com>
2022-11-05 09:42:21 +0100

    feat: created a file to hold assets relative to mood

---
+++ src/utils/consts.js
@@ -0,0 +1,22 @@
+export const MOOD_ASSETS = {
+  Awful: {
+    image: "",
+    color: getComputedStyle(document.body).getPropertyValue("--color-awful"),
+  },
+  Bad: {
+    image: "",
+    color: getComputedStyle(document.body).getPropertyValue("--color-bad"),
+  },
+  Okay: {
+    image: "",
+    color: getComputedStyle(document.body).getPropertyValue("--color-okay"),
+  },
+  Good: {
+    image: "",
+    color: getComputedStyle(document.body).getPropertyValue("--color-good"),
+  },
+  Great: {
+    image: "",
+    color: getComputedStyle(document.body).getPropertyValue("--color-great"),
+  },
+};
