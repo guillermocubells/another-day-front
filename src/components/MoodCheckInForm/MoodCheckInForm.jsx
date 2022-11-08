d4c8f90b96f9205d90f3f7f2c9aac0dbb4dd4cbf
poncho678 <vincent.tollens@googlemail.com>
2022-11-08 16:28:46 +0100

    adjusted smiley spacing

--- src/components/MoodCheckInForm/MoodCheckInForm.jsx
+++ src/components/MoodCheckInForm/MoodCheckInForm.jsx
@@ -159,29 +159,31 @@ function MoodCheckInForm() {
             {/* Get Activities. 
       TODO! create array from selected values
       TODO! create the option to create a new activity. */}
-            <h4>What are you doing?</h4>
-            <div className="mood-check-in__form-activities mood-check-in__form-flex">
-              {activities.map((activity, index) => {
-                const { _id, title } = activity;
-                return (
-                  <PillSmallCheckbox
-                    key={_id || title}
-                    title={title}
-                    handleCheckbox={handleCheckbox}
-                    checked={
-                      form.activities.length
-                        ? form.activities.some((e) => e === title)
-                        : ""
-                    }
-                  />
-                );
-              })}
-              <CreateCustomActivity
-                form={form}
-                setForm={setForm}
-                moodData={moodData}
-                setMoodData={setMoodData}
-              />
+            <div className="mood-check-in__form-activities-wrapper">
+              <h4>What are you doing?</h4>
+              <div className="mood-check-in__form-activities mood-check-in__form-flex">
+                {activities.map((activity, index) => {
+                  const { _id, title } = activity;
+                  return (
+                    <PillSmallCheckbox
+                      key={_id || title}
+                      title={title}
+                      handleCheckbox={handleCheckbox}
+                      checked={
+                        form.activities.length
+                          ? form.activities.some((e) => e === title)
+                          : ""
+                      }
+                    />
+                  );
+                })}
+                <CreateCustomActivity
+                  form={form}
+                  setForm={setForm}
+                  moodData={moodData}
+                  setMoodData={setMoodData}
+                />
+              </div>
             </div>
             <label>
               Note
