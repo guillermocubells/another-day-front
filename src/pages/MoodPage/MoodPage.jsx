04f0bc440a102bbf5c785bb0bf7fc15d00d5f28d
Guillermo Cubells <guillermocubells@gmail.com>
2022-11-09 20:47:41 +0100

    feat: styling to the mood page

--- src/pages/MoodPage/MoodPage.jsx
+++ src/pages/MoodPage/MoodPage.jsx
@@ -4,6 +4,8 @@ import Loading from "../../components/Loading/Loading";
 import apiClient from "../../services/api-client";
 import MoodPageActivities from "../../components/MoodListActivities/MoodListActivities";
 import { dateFormat } from "../../utils/date-helper";
+import "./MoodPage.css";
+import "../../components/MoodCheckInForm/MoodCheckInForm.css";
 
 function MoodPage() {
   const [data, setData] = useState({});
@@ -43,26 +45,43 @@ function MoodPage() {
   // console.log(activities);
   return (
     <div>
-      <article key={_id}>
-        <h5>{dateFormat(date)}</h5>
-        <h2>{status}</h2>
-        {substatus && <h3>{substatus[status]}</h3>}
-        {activities && (
-          <h4>{activities.map((activity) => activity.title).join(" ")}</h4>
-        )}
-        {journal && (
-          <article>
-            <h3>Journal</h3>
-            <div>{journal}</div>
-          </article>
-        )}
-        {/* <image /> */}
-      </article>
-      <button type="button" onClick={moveToEditPage}>
-        Edit
-      </button>
-      <div>
-        <MoodPageActivities />
+      <div className="mood-layout">
+        <div>
+          <div className="layout">
+            <article key={_id}>
+              <div>
+                <h5 className="italics">{dateFormat(date)}</h5>
+              </div>
+              <h2>{status}</h2>
+              {substatus && <h3>{substatus[status]}</h3>}
+              {activities && (
+                <h4>
+                  {activities.map((activity) => activity.title).join(" ")}
+                </h4>
+              )}
+              {journal && (
+                <article>
+                  <h3>Journal</h3>
+                  <div>{journal}</div>
+                </article>
+              )}
+              {/* <image /> */}
+            </article>
+          </div>
+          <div>
+            <button
+              className="edit-button btn-submit"
+              type="button"
+              onClick={moveToEditPage}
+            >
+              Edit
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <MoodPageActivities />
+        </div>
       </div>
     </div>
   );
