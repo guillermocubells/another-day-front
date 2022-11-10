a43f1a79006f26704cd4f09c4a54f2a565591e38
poncho678 <vincent.tollens@googlemail.com>
2022-11-10 08:38:24 +0100

    whatever mane

--- src/components/MoodListItem/MoodListItem.jsx
+++ src/components/MoodListItem/MoodListItem.jsx
@@ -1,14 +1,15 @@
 import "./MoodListItem.css";
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { MOOD_ASSETS } from "../../utils/consts";
 import PillSmall from "../Pills/PillSmall";
+
 function MoodListItem({ mood }) {
+  const navigate = useNavigate();
   const { date, status, _id, activities } = mood;
   const newDate = new Date(date);
   const { image, color } = MOOD_ASSETS[status];
-  const navigate = useNavigate();
+
   return (
     <div
       className="mood-list__item"
@@ -18,6 +19,7 @@ function MoodListItem({ mood }) {
       <div className="mood-list__item-smiley_wrapper">
         <img src={image} alt={`${status} smiley`} className="smiley-image" />
       </div>
+
       <div className="mood-list__item-info_wrapper">
         <h3 className="mood-list__item__status">
           {status}{" "}
@@ -28,6 +30,7 @@ function MoodListItem({ mood }) {
             })}
           </span>
         </h3>
+
         <div className="mood-list__item__activities">
           {activities.map((activity) => {
             return (
